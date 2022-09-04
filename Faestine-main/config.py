130ad062c7413077d39319c9c3d6c03c8062a530
PottuGD <83907925+PottuGD@users.noreply.github.com>
2022-09-04 18:16:57 +0300

    Update config.py

--- Faestine-main/config.py
+++ Faestine-main/config.py
@@ -6,5 +6,5 @@ load_dotenv()
 
 CLIENT_TOKEN = os.getenv('TOKEN')
 CLIENT_PREFIX = '&'
-GIPHY_TOKEN = os.getenv("vYBe9IHJCEBFM5Zh1kuKfrj6Wzdkd1H2")
-TENOR_TOKEN = os.getenv('AIzaSyCS11EStVU1dUtYIxNLqqP6dV75ipxLdcE')
+GIPHY_TOKEN = os.getenv("GIPHY_TOKEN")
+TENOR_TOKEN = os.getenv('TENOR_TOKEN')
